f119ee3d3ccec0039ac91acb4439167c60d6d6bd
aliraad79 <a.ahmadi.k.79@gmail.com>
2020-08-30 23:09:29 +0430

    add some graphic stuff

--- main.py
+++ main.py
@@ -2,26 +2,48 @@ import datetime
 import time, math
 from classs import Account
 import sqlite3
-import PySimpleGUI as gu
+import PySimpleGUI as sg
 
 # Database stuff
 conn = sqlite3.connect('cards.db')
 c = conn.cursor()
 
-print("\tMENU\n\tAvailable Accounts")
+# GUi stuff
+sg.theme('DarkAmber')  # Add a touch of color
+
+# All the stuff inside your window.
 a = Account('ali')
 b = Account("mahdi")
+list_of_accounts = ['new account']
+for i in Account.instances:
+    list_of_accounts.append(i.name)
+layout = [[sg.Text("Available Accounts")],
+          [sg.Listbox(list_of_accounts , size=(30, 6))],
+          [sg.Button('Ok')]]
 
-for counter, i in enumerate(Account.instances):
-    print('   ', counter + 1, i.name)
-print()
+# Create the Window
+window = sg.Window('Lightner Box', layout)
 
-inp = int(input("\tenter account number or 0 for making new account:\t"))
+# Event Loop to process "events" and get the "values" of the inputs
 
-if inp != 0:
-    account = Account.instances[inp - 1]
+event, values = window.read()
+if values[0] != 'new account':
+    for i in Account.instances:
+        if i.name == values[0]:
+            account = i
+            break
 else:
     account = Account(input('\tEnter Account name:\t'))
+
+window.close()
+layout = [[sg.Button("make new card")],
+          [sg.Button("start your practice(show cards)")],
+          [sg.Button("add card to your practice")],
+          [sg.Button('Ok')]]
+
+# Create the Window
+window = sg.Window('Lightner Box2', layout)
+
 
 inp = int(input(
     "\tWelcome!!!\n\tAccount Menu\n\t1.make new card\n\t2.start your practice(show cards)\n\t3.add card to your practice\n\tchoose:\t"))
