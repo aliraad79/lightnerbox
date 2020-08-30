4bce5649ee20b446a2a800007e7bb904f0b460c9
aliraad79 <a.ahmadi.k.79@gmail.com>
2020-08-30 16:46:17 +0430

    add starting files 2

--- classs.py
+++ classs.py
@@ -1,14 +1,29 @@
 import time
+import weakref
+
+
+def assign_cards(account):
+    cards = []
+    with open('cards/' + account.name + '.txt', 'r') as file:
+        lines = file.readlines()
+        for i in range(0, len(lines), 3):
+            cards.append(Card(account, lines[i + 1], lines[i + 2]))
+    return cards
 
 
 class Account:
+    instances = []
+
     def __init__(self, name):
         self.name = name
-        self.cards = open('cards/' + name, 'r+').readlines()
         self.created_time = time.ctime()
+        self.__class__.instances.append(weakref.proxy(self))
+        self.cards = assign_cards(self)
 
 
 class Card:
-    def __init__(self, owner: Account):
+    def __init__(self, owner: Account, title, description):
         self.owner = owner
         self.creating_time = time.ctime()
+        self.title = title
+        self.description = description
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import time, math
 from classs import Account
+
 print("\tMENU\n\tAvailable Accounts")
-for i in Account.
+a = Account('ali')
+b = Account("mahdi")
+
+for counter, i in enumerate(Account.instances):
+    print('   ', counter + 1, i.name)
+print()
+
+inp = int(input("\tenter account number or 0 for making new account:\t"))
+
+if inp != 0:
+    account = Account.instances[inp]
+else:
+    account = Account(input('\tEnter Account name:\t'))
+
+inp = input("\tWelcome!!!\n\tAccount Menu\n\t1.make new card\n\t2.start your practice\n\tchoose:\t")
+if inp == 1:
+    pass  # go
+elif inp == 2:
+    time
