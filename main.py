a90172c7c330388317e5e3d998079be7020d810c
aliraad79 <a.ahmadi.k.79@gmail.com>
2020-08-30 20:38:27 +0430

    add sqlit3 as database and PySimpleGUI as gui

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import time, math
 from classs import Account
+import sqlite3
+import PySimpleGUI as gu
+
+# Database stuff
+conn = sqlite3.connect('cards.db')
+c = conn.cursor()
 
 print("\tMENU\n\tAvailable Accounts")
 a = Account('ali')
@@ -18,6 +24,8 @@ else:
 
 inp = input("\tWelcome!!!\n\tAccount Menu\n\t1.make new card\n\t2.start your practice\n\tchoose:\t")
 if inp == 1:
-    pass  # go
+    title = input('enter title:\t')
+    desc = input('enter desc:\t')
+    c.execute("INSERT INTO cards VALUES (" + title + "," + desc + ")")
 elif inp == 2:
-    time
+    time.time()
